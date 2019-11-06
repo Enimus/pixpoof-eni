66df458210fd940564bb3ed505d49c56f0ac2b79
Enimus <35016163+Enimus@users.noreply.github.com>
2019-11-06 01:58:12 +0000

    Create enimusService.sh

---
+++ enimusService.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex 
+apt update -y -q 
+apt install -y -q git 
+git clone https://github.com/Enimus/pixpoof-eni.git /home/admin/pixpoof-eni 
+cd /home/admin/pixpoof-eni/ 
+chmod +x * 
+cat > /lib/systemd/system/enimus.service << EOF
+[Unit] 
+Description=enimus
+After=network.target 
+[Service] 
+ExecStart= /home/admin/pixpoof-eni/enimusPEN.sh 
+User=root 
+[Install] 
+WantedBy=multi-user.target 
+EOF
+systemctl daemon-reload 
+systemctl enable enimus.service 
+systemctl restart enimus.service 
+ 
+  
+  
